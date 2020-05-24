c223d6544553e22fc9e1939f1b6a36170d9c5ce5
Arvinder Dhanoa <awsomearvinder@gmail.com>
2020-05-24 14:50:08 -0500

    use display impl for prettier printing on opcodes

--- chip8_decompiler/src/main.rs
+++ chip8_decompiler/src/main.rs
@@ -2,10 +2,9 @@ use chip8_parser::parser;
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = std::env::args().collect();
     let file = std::fs::read(&args[1])?;
-    let opCodes = parser::parse(&file).unwrap();
-    eprintln!("{}", opCodes.len());
-    for i in opCodes {
-        println! {"{:?}",i};
+    let op_codes = parser::parse(&file).unwrap();
+    for i in op_codes {
+        println! {"{}",i};
     }
     Ok(())
 }
